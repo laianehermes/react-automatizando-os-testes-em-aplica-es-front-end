9b5ad9e507c5f684d9de1b716b169f31531ed59a
lsouza <lsouza@luxfacta.com>
2021-07-14 15:49:24 -0300

    create snapshot test and simulate events

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import {
+  fireEvent,
+  getByLabelText,
+  getByTestId,
+  getByText,
+  render,
+  screen,
+} from "@testing-library/react";
 import App, { calcularNovoSaldo } from "./App";
 
 describe("Componente principal", () => {
@@ -39,9 +46,25 @@ describe("Componente principal", () => {
       const saldo = 150;
       const novoSaldo = calcularNovoSaldo(valores, saldo);
 
-      const novoSaldoEPositivo = (novoSaldo >= 0) ? true : false;
+      const novoSaldoEPositivo = novoSaldo >= 0 ? true : false;
 
       expect(novoSaldoEPositivo).toBe(true);
+    });
+    it("que é um saque, a transação precisa ser realizada", () => {
+      render(<App />);
+
+      const saldo = screen.getByText("R$ 1000");
+      const transacao = screen.getByLabelText("Saque");
+      const valor = screen.getByTestId("valor");
+      const botaoTransacao = screen.getByText("Realizar operação");
+      
+      expect(saldo.textContent).toBe("R$ 1000");
+
+      fireEvent.click(transacao, { target: { value: "saque" } });
+      fireEvent.change(valor, { target: { value: 10 } });
+      fireEvent.click(botaoTransacao);
+
+      expect(saldo.textContent).toBe("R$ 990");
     });
   });
 });

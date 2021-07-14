0e7d027dff46223ac4cec1acaac342bc8634caef
lsouza <lsouza@luxfacta.com>
2021-07-14 15:06:01 -0300

    test withdrawal operation

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,47 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App, { calcularNovoSaldo } from "./App";
 
 describe("Componente principal", () => {
   describe("Quando eu abro o app do banco", () => {
-    it("O nome é exibido", () => {
+    it("o nome é exibido", () => {
       render(<App />);
 
       expect(screen.getByText("ByteBank")).toBeInTheDocument();
     });
 
-    it("O saldo é exibido", () => {
+    it("o saldo é exibido", () => {
       render(<App />);
 
       expect(screen.getByText("Saldo:")).toBeInTheDocument();
     });
-    it("O botão de realizar transação é exibido", () => {
+    it("o botão de realizar transação é exibido", () => {
       render(<App />);
 
       expect(screen.getByText("Realizar operação")).toBeInTheDocument();
+    });
+  });
+
+  describe("Quando eu realizo uma transação", () => {
+    it("que é um saque, o valor vai diminuir", () => {
+      const valores = {
+        transacao: "saque",
+        valor: 50,
+      };
+      const novoSaldo = calcularNovoSaldo(valores, 150);
+      expect(novoSaldo).toBe(100);
+    });
+    it("que é um saque, eu preciso de saldo na conta", () => {
+      const valores = {
+        transacao: "saque",
+        valor: 150,
+      };
+      const saldo = 150;
+      const novoSaldo = calcularNovoSaldo(valores, saldo);
+
+      const novoSaldoEPositivo = (novoSaldo >= 0) ? true : false;
+
+      expect(novoSaldoEPositivo).toBe(true);
     });
   });
 });
